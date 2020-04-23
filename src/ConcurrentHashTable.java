d113aebded2c221ff81bdd48f90803af6e0e2e9d
Jun Wang <1127wangjun@gmail.com>
2020-04-22 22:01:10 -0500

    fix put

--- src/ConcurrentHashTable.java
+++ src/ConcurrentHashTable.java
@@ -5,7 +5,7 @@ public interface ConcurrentHashTable<K, V> {
 
     public int hash(final K key,final V value);
 
-    public Boolean put(final V value);
+    public Boolean put(final K key, final V value);
 
     public V get(final K key);
 }
