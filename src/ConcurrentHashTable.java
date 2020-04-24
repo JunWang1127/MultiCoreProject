9a7bc1a47b025b8e60e67be89ec29e0465c1d454
Jun Wang <1127wangjun@gmail.com>
2020-04-23 23:24:26 -0500

    Add lock free hashMap and update interface

--- src/ConcurrentHashTable.java
+++ src/ConcurrentHashTable.java
@@ -1,11 +1,42 @@
 /**
- * the interface for concurrent hash tables
+ * the interface for concurrent hash tables.
  */
 public interface ConcurrentHashTable<K, V> {
 
-    public int hash(final K key,final V value);
+    /**
+     * hash function for the hashMap.
+     */
+    int hash(final K key,final V value);
 
-    public Boolean put(final K key, final V value);
+    /**
+     * put key value into map. If already contains key in map,
+     * will replace the old value by new value.
+     *
+     * @return V is the previous value associate with key,
+     * return null if the key it not in the map previously.
+     */
+     V put(final K key, final V value);
 
-    public V get(final K key);
+    /**
+     * get value based on input key
+     */
+     V get(final K key);
+
+    /**
+     * remove the key-value entry if the key present in map.
+     * @return V is the value of the key.
+     */
+     V remove(final K key);
+
+    /**
+     * remove the key-value entry if the input key-value entry present in map.
+     * @return V return true if remove successfully, else return false.
+     */
+     boolean remove(final K key, final V value);
+
+     int size();
+
+     boolean isEmpty();
+
+     boolean containsKey(final K key);
 }
---
+++ src/LockFree/Node.java
@@ -0,0 +1,32 @@
+package LockFree;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * Entry node for hash map
+ */
+public class Node<K, V> {
+
+    public V value;
+    public K key;
+    public AtomicStampedReference<Node<K, V>> next; // use stamped to avoid ABA
+    public AtomicBoolean isDeleted;
+    public Integer priority;
+
+    public Node() {
+        this.value = null;
+        this.key = null;
+        next = new AtomicStampedReference<>(null, 0);
+        isDeleted = new AtomicBoolean(false);
+    }
+
+    public Node(K key, V value) {
+        this.value = value;
+        this.key = key;
+        next = new AtomicStampedReference<>(null, 0);
+        isDeleted = new AtomicBoolean(false);
+        // the different object may have same value, thus we also has to compare value
+        priority = value.hashCode();
+    }
+}
