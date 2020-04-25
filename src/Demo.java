9ad383a036df10384e00b01f444baa2f97475854
dldbb <xiyu_wang@utexas.edu>
2020-04-24 22:35:21 -0500

    Add Chain LockBasedHashTable

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,13 @@
 import ConcurrentHashTable.ConcurrentHashTable;
+import LockBased.LockBasedHashTable;
 import LockFree.LockFreeHashMap;
 
 public class Demo {
     public static void main(String[] args) {
 
-        ConcurrentHashTable<String, String> c = new LockFreeHashMap<>();
-
+        //ConcurrentHashTable<String, String> c = new LockFreeHashMap<>();
+        ConcurrentHashTable<String, String> c = new LockBasedHashTable<>();
+        boolean b_empty = c.isEmpty();
         c.put("jun","123");
         c.put("jun","321");
         c.put("shuqi","321");
@@ -18,8 +20,10 @@ public class Demo {
         boolean e = c.containsKey("shuqi");
         c.remove("shuqi");
         c.remove("jun","123");
-        c.remove("jun","321");
+        //c.remove("jun","321");
+        c.remove("haha");
 
-        System.out.println(c.get("jun"));
+        //System.out.println(c.get("jun"));
+        System.out.println(c.values());
     }
 }
